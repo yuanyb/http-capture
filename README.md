18c1e6f7a2aa52c81e4d16423e40701d8efe3f3f
iyuanyb <iyuanyb@foxmail.com>
2020-10-27 21:32:02 +0800

    README.md

---
+++ README.md
@@ -0,0 +1,30 @@
+Go实现的一个命令行HTTP抓包工具
+
+> 可以对HTTP（暂不支持HTTPS）请求做拦截，并修改请求信息。
+> 只会拦截POST请求及部分非GET请求，对一些JS、CSS等静态资源请求不会拦截。
+
+### 命令：
+- **start:**
+    - start                  进入抓包拦截状态
+- **exit:**
+    - exit                   退出程序
+- **release:**
+    - release                                释放所有请求
+- **list:**
+    - list request           列出当前拦截的所有请求及对应ID
+    - list header -id reqId  列出某个请求的所有Header
+- **get:**
+    - get header -id reqId -h key   获取某个请求的某个Header
+    - get param -id reqId -p key    获取某个请求的某个参数(GET&POST)
+    - get cookie -id reqId \[-c key] 获取某个请求的Cookie，不提供-c则获取所有Cookie
+    - get body -id reqId            获取某个请求的Body，仅对POST请求有效
+- **set:**
+    - set header -id reqId -v k=v      设置某个请求的某个Header
+    - set get-param -id reqId -v k=v   设置某个请求的GET参数
+    - set post-param -id reqId -v k=v  设置某个请求的POST参数，仅对表单请求有效
+    - set cookie -id reqId -v val      设置某个请求的Cookie
+    - set body -id reqId -v val        设置某个请求的Body，仅对POST请求有效，可用于修改json
+    
+### 演示：
+
+![hc](md_image/hc.png)
--- src/http_capture/cmd.go
+++ src/http_capture/cmd.go
@@ -47,10 +47,10 @@ func start() {
 }
 
 func help() {
-    fmt.Println(`start:
+	fmt.Println(`start:
     start                  进入抓包拦截状态
 exit:
-	exit                   退出程序
+    exit                   退出程序
 release:
     release				   释放所有请求
 list:
@@ -58,15 +58,15 @@ list:
     list header -id reqId  列出某个请求的所有Header
 get:
     get header -id reqId -h key   获取某个请求的某个Header
-	get param -id reqId -p key    获取某个请求的某个参数(GET&POST)
-	get cookie -id reqId [-c key] 获取某个请求的Cookie，不提供-c则获取所有Cookie
-	get body -id reqId            获取某个请求的Body，仅对POST请求有效
+    get param -id reqId -p key    获取某个请求的某个参数(GET&POST)
+    get cookie -id reqId [-c key] 获取某个请求的Cookie，不提供-c则获取所有Cookie
+    get body -id reqId            获取某个请求的Body，仅对POST请求有效
 set:
     set header -id reqId -v k=v      设置某个请求的某个Header
-	set get-param -id reqId -v k=v   设置某个请求的GET参数
-	set post-param -id reqId -v k=v  设置某个请求的POST参数，仅对表单请求有效
-	set cookie -id reqId -v val      设置某个请求的Cookie
-	set body -id reqId -v val        设置某个请求的Body，仅对POST请求有效，可用于修改json`)
+    set get-param -id reqId -v k=v   设置某个请求的GET参数
+    set post-param -id reqId -v k=v  设置某个请求的POST参数，仅对表单请求有效
+    set cookie -id reqId -v val      设置某个请求的Cookie
+    set body -id reqId -v val        设置某个请求的Body，仅对POST请求有效，可用于修改json`)
 }
 
 func list(cmd string) {
@@ -76,7 +76,7 @@ func list(cmd string) {
 	}
 	args := cmdToArgs(cmd)
 	switch {
-	case len(args) > 1 && args[1] ==  "header":
+	case len(args) > 1 && args[1] == "header":
 		headerCmd := flag.NewFlagSet("header", flag.ContinueOnError)
 		var id int
 		headerCmd.IntVar(&id, "id", -1, "-id reqId")
@@ -85,7 +85,7 @@ func list(cmd string) {
 			headerCmd.Usage()
 			return
 		}
-		if err != nil || id == - 1 {
+		if err != nil || id == -1 {
 			return
 		}
 		header := getReqList().getReq(int32(id)).Header
@@ -130,26 +130,26 @@ func set(cmd string) {
 			req.Header[k] = []string{v}
 		})
 	case "get-param":
-	    setKV(args, func(id int32, k, v string) {
-	    	if ok, _ := regexp.MatchString(`[\w-]+`, k); !ok{
-	    		fmt.Println("Invalid Command.")
-	    		return
+		setKV(args, func(id int32, k, v string) {
+			if ok, _ := regexp.MatchString(`[\w-]+`, k); !ok {
+				fmt.Println("Invalid Command.")
+				return
 			}
 			v = url.QueryEscape(v)
 			req := getReqList().id2req[id].req
-	        re := regexp.MustCompile(k + `=(.+?)(&|$)`)
-			req.URL.RawQuery = re.ReplaceAllString(req.URL.RawQuery, k + "=" + v) // 实际有效修改
-			req.RequestURI = re.ReplaceAllString(req.RequestURI, k + "=" + v) // 为了list时能够显示出来修改后的内容
+			re := regexp.MustCompile(k + `=(.+?)(&|$)`)
+			req.URL.RawQuery = re.ReplaceAllString(req.URL.RawQuery, k+"="+v) // 实际有效修改
+			req.RequestURI = re.ReplaceAllString(req.RequestURI, k+"="+v)     // 为了list时能够显示出来修改后的内容
 		})
 	case "post-param":
-	    setKV(args, func(id int32, k, v string) {
+		setKV(args, func(id int32, k, v string) {
 			req := getReqList().id2req[id].req
 			if req.Header.Get("Content-Type") != "application/x-www-form-urlencoded" {
 				fmt.Println("Only available for POST form request.")
 				return
 			}
 			body, _ := ioutil.ReadAll(req.Body)
-			body = regexp.MustCompile(k + `=(.+?)(&|$)`).ReplaceAll(body, []byte(k + "=" + v))
+			body = regexp.MustCompile(k+`=(.+?)(&|$)`).ReplaceAll(body, []byte(k+"="+v))
 			req.Body = ioutil.NopCloser(bytes.NewReader(body))
 		})
 	case "cookie":
@@ -194,10 +194,10 @@ func parseSetCmd(args []string) (int32, string) {
 	return int32(id), val
 }
 
-func setKV(args []string, action func(id int32, k, v string))  {
+func setKV(args []string, action func(id int32, k, v string)) {
 	if id, val := parseSetCmd(args); id != -1 {
 		if id == -2 { // 无效id
-		    return
+			return
 		}
 		if ok, _ := regexp.MatchString(`.+=.+`, val); ok {
 			i := strings.IndexByte(val, '=')
@@ -224,11 +224,11 @@ func get(cmd string) {
 	switch args[1] {
 	case "header":
 		if id, key := parseGetCmd(args, "h"); id != -1 {
-		    if id == -2 { // 无效的id
-		        break
+			if id == -2 { // 无效的id
+				break
 			}
 			for i, v := range getReqList().id2req[int32(id)].req.Header[key] {
-				fmt.Println("Value-" + strconv.Itoa(i) + ":", v)
+				fmt.Println("Value-"+strconv.Itoa(i)+":", v)
 			}
 		}
 	case "param":
@@ -239,7 +239,7 @@ func get(cmd string) {
 			req := getReqList().id2req[int32(id)].req
 			_ = req.ParseForm()
 			for i, v := range req.Form[key] {
-				fmt.Println("Value-" + strconv.Itoa(i) + ":", v)
+				fmt.Println("Value-"+strconv.Itoa(i)+":", v)
 			}
 		}
 	case "cookie":
